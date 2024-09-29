21fe111e01c13d0c51b47322f510fcbc72b93ee4
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2024-09-29 13:17:36 +0300

    тест буилд

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern-compiler",
-      },
-    },
-  },
   build: {
     outDir: "dist",
   },
